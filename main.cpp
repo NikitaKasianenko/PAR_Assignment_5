8e04b68f8ea5aa23dc66abc723bd4d0839d8d98a
NikitaKasianenko <nikkas2016@gmail.com>
2024-07-15 12:28:57 +0300

    rebuiled code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <stack>
+#include <unordered_map>
+#include <sstream>
 
 using namespace std;
 
@@ -47,3 +49,172 @@ private:
     }
 };
 
+QueueList Tokenizated(const string& input) {
+    QueueList tokens;
+    string curToken;
+
+    for (char ch : input) {
+        if (ch == '.' || isdigit(ch) || isalpha(ch)) {
+            curToken += ch;
+        }
+        else if (ch == '*' || ch == '+' || ch == '-' || ch == ',' || ch == '*' || ch == ')' || ch == '(' || ch == '^' || ch == '/') {
+            if (!curToken.empty()) {
+                tokens.Add(curToken);
+                curToken.clear();
+            }
+            tokens.Add(string(1, ch));
+        }
+    }
+
+    if (!curToken.empty()) {
+        tokens.Add(curToken);
+    }
+
+    return tokens;
+}
+
+QueueList ShuntingYard(const QueueList& allTokens) {
+    QueueList queue;
+    stack<string> stack;
+
+    unordered_map<string, int> priority = {
+        {"+", 1}, {"-", 1}, {"*", 2}, {"/", 2}, {"^", 3}, {"(", 0}, {")", 0}, {"m", 4}, {"M", 4},{"a", 4},
+    };
+
+    for (const auto& token : allTokens) {
+        double token_double;
+        istringstream iss(token);
+        bool isNumber = (iss >> token_double) ? true : false;
+
+        if (isNumber) {
+            queue.Add(token);
+        }
+        else {
+            if (token == "(") {
+                stack.push(token);
+            }
+            else if (token == ",") {
+                while (!stack.empty() && stack.top() != "(") {
+                    queue.Add(stack.top());
+                    stack.pop();
+                }
+            }
+            else if (token == ")") {
+                while (!stack.empty() && stack.top() != "(") {
+                    queue.Add(stack.top());
+                    stack.pop();
+                }
+                stack.pop();
+            }
+            else {
+                while (!stack.empty() && priority[stack.top()] >= priority[token]) {
+                    queue.Add(stack.top());
+                    stack.pop();
+                }
+                stack.push(token);
+            }
+        }
+    }
+
+    while (!stack.empty()) {
+        queue.Add(stack.top());
+        stack.pop();
+    }
+
+    return queue;
+}
+
+double max(double a, double b) {
+    return (a > b) ? a : b;
+}
+
+double min(double a, double b) {
+    return (a < b) ? a : b;
+}
+
+string Calculation(QueueList& shuntingYard) {
+    stack<string> resultStack;
+
+    for (const auto& element : shuntingYard) {
+        double digit;
+        istringstream iss(element);
+        if (iss >> digit) {
+            resultStack.push(element);
+        }
+        else {
+      
+            double digit2 = stod(resultStack.top());
+            resultStack.pop();
+            double digit1 = stod(resultStack.top());
+            resultStack.pop();
+
+            if (element == "+") {
+                resultStack.push(to_string(digit1 + digit2));
+            }
+            else if (element == "-") {
+                resultStack.push(to_string(digit1 - digit2));
+            }
+            else if (element == "*") {
+                resultStack.push(to_string(digit1 * digit2));
+            }
+            else if (element == "/") {
+                resultStack.push(to_string(digit1 / digit2));
+            }
+            else if (element == "^") {
+                resultStack.push(to_string(pow(digit1, digit2)));
+            }
+            else if (element == "m") {
+                resultStack.push(to_string(min(digit1, digit2)));
+            }
+            else if (element == "M") {
+                resultStack.push(to_string(max(digit1, digit2)));
+            }
+            
+        }
+    }
+
+    return resultStack.top();
+}
+
+string replaceFunctions(const string& expression) {
+    string result = expression;
+
+    size_t pos = result.find("min(");
+    while (pos != string::npos) {
+        result.replace(pos, 4, "m(");
+        pos = result.find("min(", pos + 1);
+    }
+
+    pos = result.find("max(");
+    while (pos != string::npos) {
+        result.replace(pos, 4, "M(");
+        pos = result.find("max(", pos + 1);
+    }
+    pos = result.find("abs(");
+    while (pos != string::npos) {
+        result.replace(pos, 4, "a(");
+        pos = result.find("abs(", pos + 1);
+    }
+
+    return result;
+}
+
+int main() {
+    while (true) {
+        string expression;
+        cout << "Enter expression: ";
+        getline(cin, expression);
+
+        if (expression.compare("exit") == 0) {
+            break;
+        }
+
+        expression = replaceFunctions(expression);
+        QueueList tokens = Tokenizated(expression);
+        QueueList rpn = ShuntingYard(tokens);
+        string result = Calculation(rpn);
+
+        cout << "Result: " << result << endl;
+    }
+    return 0;
+}
